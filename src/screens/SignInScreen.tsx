9a29f6321bea6e94e1ac053a1187965d592c20e3
hykepashi <adrian.hoff2005@gmail.com>
2023-05-13 15:37:55 -0300

    feat: SignInScreen/ Add email and password inputs into it

--- src/screens/SignInScreen.tsx
+++ src/screens/SignInScreen.tsx
@@ -1,12 +1,16 @@
+//native-base
 import { VStack, Image, Text, Center, Heading } from "native-base";
 
 //assets
 import BackgroundImg from "@assets/background.png";
 import LogoSvg from "@assets/logo.svg";
 
+//components
+import { InputComponent } from "@components/InputComponent";
+
 export function SignInScreen() {
   return (
-    <VStack flex={1} bg={"gray.700"}>
+    <VStack flex={1} bg={"gray.700"} px={10}>
       <Image
         source={BackgroundImg}
         alt="SignInScreen/ people trainning"
@@ -21,15 +25,17 @@ export function SignInScreen() {
       </Center>
 
       <Center>
-        <Heading
-          fontFamily={"heading"}
-          color={"gray.100"}
-          fontSize={"xl"}
-          mb={6}
-        >
+        <Heading fontFamily="heading" color="gray.100" fontSize="xl" mb={6}>
           Access your account
         </Heading>
       </Center>
+
+      <InputComponent
+        placeholder="Email"
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <InputComponent placeholder="Password" secureTextEntry />
     </VStack>
   );
 }
