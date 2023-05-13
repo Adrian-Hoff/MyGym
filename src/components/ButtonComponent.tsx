33d769c01b5dcfcf6074708ff0d2cdfdb38772c7
hykepashi <adrian.hoff2005@gmail.com>
2023-05-13 16:07:38 -0300

    feat: ButtonComponent/ Add outline variant into this component

--- src/components/ButtonComponent.tsx
+++ src/components/ButtonComponent.tsx
@@ -2,21 +2,34 @@ import { Button, IButtonProps, Text } from "native-base";
 
 type Props = IButtonProps & {
   title: string;
+  variant?: "solid" | "outline";
 };
 
-export function ButtonComponent({ title, ...rest }: Props) {
+export function ButtonComponent({ title, variant = "solid", ...rest }: Props) {
   return (
     <Button
       rounded={"sm"}
       w={"full"}
       h={14}
-      bg={"violet.500"}
-      _pressed={{
-        bg: "violet.900",
-      }}
+      bg={variant === "outline" ? "transparent" : "violet.500"}
+      borderWidth={variant === "outline" ? 1 : 0}
+      borderColor={"violet.600"}
+      _pressed={
+        variant === "outline"
+          ? {
+              bg: "gray.500",
+            }
+          : {
+              bg: "violet.900",
+            }
+      }
       {...rest}
     >
-      <Text color={"white"} fontFamily={"heading"} fontSize={"sm"}>
+      <Text
+        color={variant === "outline" ? "violet.600" : "white"}
+        fontFamily={"heading"}
+        fontSize={"sm"}
+      >
         {title}
       </Text>
     </Button>
