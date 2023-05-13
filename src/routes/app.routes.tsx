1227c604450c41f2c1d5cf35daa486016c1af571
hykepashi <adrian.hoff2005@gmail.com>
2023-05-13 19:57:22 -0300

    feat: AppRoutes/ add style into tab

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,4 +1,5 @@
-import * as React from "react";
+//react-native
+import { Platform } from "react-native";
 
 //react-navigation
 import {
@@ -11,12 +12,14 @@ import DashboardSVG from "@assets/home.svg";
 import HistorySVG from "@assets/history.svg";
 import ProfileSVG from "@assets/profile.svg";
 
+//native-base
+import { Box, useTheme } from "native-base";
+
 //screens
 import { DashboardScreen } from "@screens/DashboardScreen";
 import { ExercisesScreen } from "@screens/ExercisesScreen";
 import { HistoryScreen } from "@screens/HistoryScreen";
 import { ProfileScreen } from "@screens/ProfileScreen";
-import { Text, theme, useTheme } from "native-base";
 
 type AppRoutesTypeProps = {
   DashboardScreen: undefined;
@@ -29,15 +32,44 @@ export type AppNavigationRoutesProps =
   MaterialBottomTabScreenProps<AppRoutesTypeProps>;
 
 export function AppRoutes() {
-  const { sizes } = useTheme();
+  const { sizes, colors } = useTheme();
   const iconSize = sizes[6];
 
   const Tab = createMaterialBottomTabNavigator<AppRoutesTypeProps>();
 
   return (
-    <Tab.Navigator labeled={false}>
+    <Tab.Navigator
+      labeled={false}
+      initialRouteName="DashboardScreen"
+      activeColor={colors.violet[600]}
+      inactiveColor={colors.gray[200]}
+      // shifting={true}
+      barStyle={{
+        borderRadius: 10,
+        borderColor: colors.gray[600],
+        borderWidth: 4,
+
+        bottom: 20,
+        backgroundColor: colors.gray[600],
+        height: Platform.OS === "android" ? "auto" : 96,
+      }}
+    >
       <Tab.Screen
         options={{
+          tabBarLabel: "History",
+          tabBarTestID: "HistoryID",
+          tabBarIcon: ({ color }) => (
+            <HistorySVG fill={color} height={iconSize} width={iconSize} />
+          ),
+          tabBarAccessibilityLabel: "ok",
+        }}
+        name="HistoryScreen"
+        component={HistoryScreen}
+      />
+      <Tab.Screen
+        options={{
+          tabBarLabel: "Home",
+          tabBarTestID: "HomeID",
           tabBarIcon: ({ color }) => (
             <DashboardSVG fill={color} height={iconSize} width={iconSize} />
           ),
@@ -47,18 +79,12 @@ export function AppRoutes() {
       />
       <Tab.Screen
         options={{
-          tabBarIcon: ({ color }) => (
-            <HistorySVG fill={color} height={iconSize} width={iconSize} />
-          ),
-        }}
-        name="HistoryScreen"
-        component={HistoryScreen}
-      />
-      <Tab.Screen
-        options={{
+          tabBarLabel: "Profile",
+          tabBarTestID: "ProfileID",
           tabBarIcon: ({ color }) => (
             <ProfileSVG fill={color} height={iconSize} width={iconSize} />
           ),
+          tabBarBadge: true,
         }}
         name="ProfileScreen"
         component={ProfileScreen}
