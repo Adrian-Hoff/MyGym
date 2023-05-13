a61cdcce0f5668f099d159941b0315c221bc355c
hykepashi <adrian.hoff2005@gmail.com>
2023-05-13 17:59:13 -0300

    feat: HistoryComponent(new)/ init this screen

--- src/screens/HistoryScreen.tsx
+++ src/screens/HistoryScreen.tsx
@@ -1,9 +1,9 @@
 import { Center, Text } from "native-base";
 
-export function DashboardScreen() {
+export function HistoryScreen() {
   return (
     <Center flex={1}>
-      <Text>Dashboard</Text>
+      <Text>History</Text>
     </Center>
   );
 }
