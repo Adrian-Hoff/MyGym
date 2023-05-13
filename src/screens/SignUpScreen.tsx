4c2b5c8b23deff1cb9d67e129aa2fe3bf909cd01
hykepashi <adrian.hoff2005@gmail.com>
2023-05-13 16:27:21 -0300

    feat: SignUpScreen/ crete this screen by cloning SignInScreen and makink some changes: Add name input, remove create account text, change inputs texts, change access your account text

---
+++ src/screens/SignUpScreen.tsx
@@ -0,0 +1,61 @@
+//native-base
+import {
+  VStack,
+  Image,
+  Text,
+  Center,
+  Heading,
+  ScrollView,
+  Flex,
+} from "native-base";
+
+//assets
+import BackgroundImg from "@assets/background.png";
+import LogoSvg from "@assets/logo.svg";
+
+//components
+import { InputComponent } from "@components/InputComponent";
+import { ButtonComponent } from "@components/ButtonComponent";
+
+export function SignUpScreen() {
+  return (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} bg={"gray.700"} px={10} pb={16}>
+        <Image
+          source={BackgroundImg}
+          alt="SignInScreen/ people trainning"
+          resizeMode="contain"
+          position={"absolute"}
+        />
+        <Center my={24}>
+          <LogoSvg />
+          <Text color={"gray.100"} fontSize={"sm"}>
+            Train your mind and your body
+          </Text>
+        </Center>
+
+        <Center>
+          <Heading fontFamily="heading" color="gray.100" fontSize="xl" mb={6}>
+            Create your account
+          </Heading>
+        </Center>
+
+        <InputComponent placeholder="Name" />
+
+        <InputComponent
+          placeholder="Email"
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <InputComponent placeholder="Password" secureTextEntry />
+
+        <ButtonComponent title="Create Account" />
+
+        <ButtonComponent mt={24} title="Back To Login" variant={"outline"} />
+      </VStack>
+    </ScrollView>
+  );
+}
