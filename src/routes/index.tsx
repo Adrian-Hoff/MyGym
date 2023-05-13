5372b198c6019222353421059e3c9f72c7852d38
hykepashi <adrian.hoff2005@gmail.com>
2023-05-13 17:12:07 -0300

    refactor: add bg into navigation container theme

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,22 @@
+//native-base
+import { useTheme, Box } from "native-base";
+
 //react-navigation
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 
 //routes
 import { AuthRoutes } from "./auth.routes";
 
 export function Routes() {
+  const nativeBaseTheme = useTheme();
+  const theme = DefaultTheme;
+  theme.colors.background = nativeBaseTheme.colors.gray[700];
+
   return (
-    <NavigationContainer>
-      <AuthRoutes />
-    </NavigationContainer>
+    <Box flex={1} bg={"gray.700"}>
+      <NavigationContainer theme={theme}>
+        <AuthRoutes />
+      </NavigationContainer>
+    </Box>
   );
 }
