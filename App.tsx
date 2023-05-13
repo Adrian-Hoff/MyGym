22b42511f9015f7e817d0989911a5a18b82a1d70
hykepashi <adrian.hoff2005@gmail.com>
2023-05-13 17:02:40 -0300

    feat: App/ add loaded font verification and loadingComponent Render

--- App.tsx
+++ App.tsx
@@ -16,9 +16,18 @@ import {
 
 //native-base
 import { Box, NativeBaseProvider } from "native-base";
+import { LoadingComponent } from "@components/LoadingComponent";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  if (!fontsLoaded) {
+    return (
+      <NativeBaseProvider theme={THEME}>
+        <LoadingComponent />
+      </NativeBaseProvider>
+    );
+  }
 
   return (
     <NativeBaseProvider theme={THEME}>
