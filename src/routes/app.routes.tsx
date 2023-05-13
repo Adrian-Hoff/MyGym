8b7911adbb8d7cd265ad3a2d4425fafa88d70c24
hykepashi <adrian.hoff2005@gmail.com>
2023-05-13 18:35:50 -0300

    refactor: AppRoutes/ add icons into tab

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,14 +1,22 @@
+import * as React from "react";
+
 //react-navigation
 import {
   createMaterialBottomTabNavigator,
   MaterialBottomTabScreenProps,
 } from "@react-navigation/material-bottom-tabs";
 
+//assets
+import DashboardSVG from "@assets/home.svg";
+import HistorySVG from "@assets/history.svg";
+import ProfileSVG from "@assets/profile.svg";
+
 //screens
 import { DashboardScreen } from "@screens/DashboardScreen";
 import { ExercisesScreen } from "@screens/ExercisesScreen";
 import { HistoryScreen } from "@screens/HistoryScreen";
 import { ProfileScreen } from "@screens/ProfileScreen";
+import { Text, theme, useTheme } from "native-base";
 
 type AppRoutesTypeProps = {
   DashboardScreen: undefined;
@@ -21,14 +29,41 @@ export type AppNavigationRoutesProps =
   MaterialBottomTabScreenProps<AppRoutesTypeProps>;
 
 export function AppRoutes() {
+  const { sizes } = useTheme();
+  const iconSize = sizes[6];
+
   const Tab = createMaterialBottomTabNavigator<AppRoutesTypeProps>();
 
   return (
     <Tab.Navigator labeled={false}>
-      <Tab.Screen name="DashboardScreen" component={DashboardScreen} />
-      <Tab.Screen name="HistoryScreen" component={HistoryScreen} />
-      <Tab.Screen name="ProfileScreen" component={ProfileScreen} />
-      <Tab.Screen name="ExercisesScreen" component={ExercisesScreen} />
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({ color }) => (
+            <DashboardSVG fill={color} height={iconSize} width={iconSize} />
+          ),
+        }}
+        name="DashboardScreen"
+        component={DashboardScreen}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HistorySVG fill={color} height={iconSize} width={iconSize} />
+          ),
+        }}
+        name="HistoryScreen"
+        component={HistoryScreen}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({ color }) => (
+            <ProfileSVG fill={color} height={iconSize} width={iconSize} />
+          ),
+        }}
+        name="ProfileScreen"
+        component={ProfileScreen}
+      />
+      {/* <Tab.Screen name="ExercisesScreen" component={ExercisesScreen} /> */}
     </Tab.Navigator>
   );
 }
