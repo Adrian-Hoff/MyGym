40cf833390908a9f02d5a14e61d12af13ace41fe
hykepashi <adrian.hoff2005@gmail.com>
2023-05-14 15:09:06 -0300

    refactor: HistoryCardComponent/refactor this component style

--- src/components/ExerciseCardComponent.tsx
+++ src/components/ExerciseCardComponent.tsx
@@ -34,30 +34,28 @@ export function ExerciseCardComponent({
 }: Props) {
   return (
     <TouchableOpacity>
-      <Box borderWidth={0.5} borderColor={"gray.400"} mb={3} rounded={"md"}>
-        <HStack p={1} rounded={"md"} alignItems={"center"}>
-          <Image
-            source={{
-              uri: uri,
-            }}
-            w={size}
-            h={size}
-            rounded={"md"}
-            {...rest}
-            mr={4}
-          />
-          <VStack flex={1}>
-            <Heading color={"gray.200"} fontSize={"lg"}>
-              {title}
-            </Heading>
-            <Text color={"gray.300"} fontSize={"sm"} mt={1} numberOfLines={2}>
-              {description}
-            </Text>
-          </VStack>
+      <HStack p={1} mb={3} alignItems={"center"}>
+        <Image
+          source={{
+            uri: uri,
+          }}
+          w={size}
+          h={size}
+          rounded={20}
+          {...rest}
+          mr={4}
+        />
+        <VStack flex={1}>
+          <Heading color={"gray.200"} fontSize={"lg"}>
+            {title}
+          </Heading>
+          <Text color={"gray.300"} fontSize={"sm"} mt={1} numberOfLines={2}>
+            {description}
+          </Text>
+        </VStack>
 
-          <Icon as={Entypo} name="chevron-thin-right" color={"gray.400"} />
-        </HStack>
-      </Box>
+        <Icon as={Entypo} name="chevron-thin-right" color={"gray.400"} />
+      </HStack>
     </TouchableOpacity>
   );
 }
