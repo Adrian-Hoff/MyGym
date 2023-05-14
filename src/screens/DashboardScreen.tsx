873cf66bef034c50bf28346f0088e0dc56360c5c
hykepashi <adrian.hoff2005@gmail.com>
2023-05-14 11:48:40 -0300

    feat: DashboardScreen/ add ExerciseCardComponent inside flatlist

--- src/screens/DashboardScreen.tsx
+++ src/screens/DashboardScreen.tsx
@@ -1,7 +1,8 @@
 import { DashboardHeaderComponent } from "@components/DashboardHeaderComponents";
 import { MuscularGroupComponent } from "@components/MuscularGroupComponent";
-import { Center, FlatList, HStack, Heading, Text, VStack } from "native-base";
+import { FlatList, HStack, Heading, Text, VStack } from "native-base";
 import { useState } from "react";
+import { ExerciseCardComponent } from "./ExerciseCardComponent";
 
 export function DashboardScreen() {
   const [groups, setGroups] = useState<string[]>([
@@ -13,11 +14,20 @@ export function DashboardScreen() {
     "backward",
   ]);
 
+  const [exercises, setExercises] = useState([
+    "One Arm Row",
+    "Pull Up",
+    "Push Up",
+    "Deadlift",
+    "Bench press",
+  ]);
+
   const [selected, setSelected] = useState("chest");
 
   return (
     <VStack flex={1}>
       <DashboardHeaderComponent />
+
       <FlatList
         _contentContainerStyle={{ pl: 5, pr: 2 }}
         mb={10}
@@ -35,8 +45,8 @@ export function DashboardScreen() {
         )}
       />
 
-      <VStack flex={1} space={8} px={5}>
-        <HStack justifyContent={"space-between"}>
+      <VStack flex={1} px={5}>
+        <HStack justifyContent={"space-between"} mb={5}>
           <Heading color={"gray.200"} fontSize={"md"}>
             Exercises
           </Heading>
@@ -44,6 +54,24 @@ export function DashboardScreen() {
             4
           </Text>
         </HStack>
+
+        <FlatList
+          data={exercises}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <ExerciseCardComponent
+              title={item}
+              description="3 Sets x 12 Reps"
+              size={16}
+              alt="image"
+              uri="https://www.origym.com.br/upload/remada-unilateral-3.png"
+            />
+          )}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{
+            paddingBottom: 10,
+          }}
+        />
       </VStack>
     </VStack>
   );
