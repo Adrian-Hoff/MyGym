1736501009c8deb89180afa88fcd094bd6b15d6a
hykepashi <adrian.hoff2005@gmail.com>
2023-05-14 10:00:38 -0300

    feat: DashboardSrceen/ render MuscularGroupComponent with active verification using flatlist

--- src/screens/DashboardScreen.tsx
+++ src/screens/DashboardScreen.tsx
@@ -1,10 +1,44 @@
 import { DashboardHeaderComponent } from "@components/DashboardHeaderComponents";
-import { Center, Text, VStack } from "native-base";
+import { MuscularGroupComponent } from "@components/MuscularGroupComponent";
+import { Center, FlatList, HStack, Text, VStack } from "native-base";
+import { useState } from "react";
 
 export function DashboardScreen() {
+  const [groups, setGroups] = useState<string[]>([
+    "chest",
+    "biceps",
+    "triceps",
+    "forearms",
+    "shoulders",
+    "backward",
+  ]);
+
+  const [selected, setSelected] = useState("chest");
+
   return (
     <VStack flex={1}>
       <DashboardHeaderComponent />
+      <FlatList
+        mb={10}
+        flexGrow={0}
+        keyExtractor={(item) => item}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        data={groups}
+        renderItem={({ item }) => (
+          <MuscularGroupComponent
+            name={item}
+            variant={selected === item ? "SELECTED" : "NOT_SELECTED"}
+            onPress={() => setSelected(item)}
+          />
+        )}
+      />
+      {/* <HStack>
+        <MuscularGroupComponent variant="SELECTED" name="Chest" />
+        <MuscularGroupComponent variant="NOT_SELECTED" name="Arms" />
+        <MuscularGroupComponent variant="NOT_SELECTED" name="Legs" />
+        <MuscularGroupComponent variant="NOT_SELECTED" name="forearms" />
+      </HStack> */}
     </VStack>
   );
 }
